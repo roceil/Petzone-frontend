694201ed648a553a20bcc33818101594fae6a8d4
xinyu <jolin7180732@gmail.com>
2024-03-11 12:05:45 +0800

    fix(mypost): 修正 user API

--- src/views/MyPostView.vue
+++ src/views/MyPostView.vue
@@ -3,7 +3,7 @@ import { ref, onMounted } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
 import { useUserStore } from '@/stores/user'
 import { storeToRefs } from 'pinia'
-import { get_memberById_data_api } from '@/api/user'
+import { get_member_data_api } from '@/api/user'
 import { get_user_posts_api } from '@/api/community'
 import avatar from '@/assets/avatar.svg'
 import EditPostModal from '@/components/EditPostModal.vue'
@@ -20,7 +20,7 @@ const userData = ref({})
 // 取得使用者資料
 const getMemberData = async () => {
   try {
-    const data = await get_memberById_data_api(id)
+    const data = await get_member_data_api(id)
     userData.value = data
   } catch (error) {
     console.error(error)

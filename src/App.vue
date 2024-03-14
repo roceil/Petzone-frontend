f38d9c7d6e1de05f244d75bb2bb33a8073cf3778
HsuXinYu <jolin7180732@gmail.com>
2024-03-14 18:02:58 +0800

    feat(loading): 新增loading component

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,16 @@ import { RouterView } from 'vue-router'
 import Header from './components/Header.vue'
 import { useUserStore } from '@/stores/user'
 import { useAuthStore } from '@/stores/auth'
+import { useLoadingStore } from '@/stores/loading'
+import { storeToRefs } from 'pinia'
 import { check_user_api } from '@/api/auth'
 import Alert from '@/components/alert/Alert.vue'
+import Loading from '@/components/LoadingView.vue'
 
 const userStore = useUserStore()
 const authStore = useAuthStore()
+const LoadingStore = useLoadingStore()
+const { LoadingMode } = storeToRefs(LoadingStore)
 
 onMounted(async () => {
   try {
@@ -32,4 +37,5 @@ onMounted(async () => {
   <Header />
   <RouterView />
   <Alert />
+  <Loading v-if="LoadingMode" />
 </template>
---
+++ src/components/LoadingView.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="fixed top-0 w-screen h-screen bg-gray-50 opacity-50 z-10"></div>
+  <div class="fixed top-[30%] left-[40%] w-[20%] z-20">
+    <img src="../assets/loading_pets.svg" alt="loading_pets" />
+    <p class="text-4xl text-font text-center">Loading...</p>
+  </div>
+</template>

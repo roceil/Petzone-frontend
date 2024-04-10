dfa40f09c0910a29b89388f688ca83f074e88b14
HsuXinYu <jolin7180732@gmail.com>
2024-04-10 21:28:32 +0800

    fix(footer): 修改 footer 定位

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,11 +1,11 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-  <div class="Footer mt-10 relative">
-    <img src="../assets/footer_wave.png" alt="footer_wave" class="w-full h-[30vh]" />
+  <div class="Footer w-full mt-10 absolute">
+    <img src="../assets/footer_wave.png" alt="footer_wave" class="w-full object-cover" />
 
     <!-- 版權文字 -->
     <div
-      class="text-white space-y-[13px] absolute bottom-5 right-5 md:bottom-[45px] md:right-[58px] text-xs md:text-base"
+      class="text-white md:space-y-[12px] absolute bottom-2 right-2 md:bottom-[45px] md:right-[45px] text-xs md:text-base"
     >
       <p>© 2024 PET ZONE All Rights Reserved.</p>
       <p>本網頁僅為學習用途，不做任何商業使用</p>
